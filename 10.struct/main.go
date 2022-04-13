e3754a36f8a8cb63ad8d0152724008e164cb8838
hyun0404woo <hyun0404woo@gmail.com>
2022-04-13 21:11:13 +0900

    10. struct 자료형, 생성자

---
+++ 10.struct/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// struct 정의
+type person struct {
+	name string
+	age int
+}
+func main() {
+	// person 객체 생성 ver1
+	p := person{}
+
+	p.name = "Lee"
+	p.age = 10
+
+	fmt.Println(p)
+
+	// person 객체 생성 ver2
+	var p1 person
+	p1 = person{"Bob", 20}
+	p2 := person{name: "Son", age: 50}
+	fmt.Println(p1, p2)
+
+	// person 객체 생성 ver3
+	// new 를 사용하면 모든 필드를 Zero value 로 초기화 하고,
+	// person 객체의 포인터 (*person) 를 리턴한다.
+	p3 := new(person)
+	// p는 포인터지만 '->' 기호가 아닌 '.' 을 사용한다.
+	p3.name = "Lee"
+	fmt.Println(p3) // &{Lee 0}
+
+	// 생성자 (constructor)
+	// struct 객체를 만들 때 
+	dic := newDict()
+	dic.data[1] = "A"
+	fmt.Println(dic)  // &{map[1:A]}
+
+}
+
+type dict struct {
+	data map[int]string
+}
+
+func newDict() *dict{
+	d := dict{}
+	d.data = map[int]string{}
+	return &d
+}
+
