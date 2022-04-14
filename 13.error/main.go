7ac8cc2d46b1e834d49ccaa9263cc34791db8864
hyun0404woo <hyun0404woo@gmail.com>
2022-04-15 00:22:57 +0900

    커스텀 에러 코드 수정

--- 13.error/main.go
+++ 13.error/main.go
@@ -1,44 +1,46 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
-	"os"
 )
 
 // Go 에러는 error 인터페이스를 통해서 주고 받는다.
 // interface 는 Error() 메서드 하나를 갖는다.
 // 이 인터페이스를 구현하는 커스텀 에러 타입을 만들 수 있다.
 
-type myError struct {
-	Code string
+// 문자열을 반환해주는 Error() 메소드를 갖는 객체들은
+// 모두 에러 타입이 될 수 있다.
+
+type CustomError struct {
+	Code    string
 	Message string
 }
 
-func (e *myError) Error() string {
+func (e *CustomError) Error() string {
 	return e.Code + ", " + e.Message
 }
 
-func do() error{
-	return &myError{Code:"400", Message:"bad request"}
+func isSame(a int, b int) (bool, error) {
+	if a != b {
+		return false, &CustomError{Code: "400", Message: "Not same"}
+	}
+	return true, nil
 }
-
-func do2() error{
-	return errors.New("error!!")
-}
-
 
 func main() {
-	f, err := os.Open("./temp.txt")
-	if err != nil {
-		log.Fatal(err.Error())
-	} else {
-		fmt.Println(f.Name())
-	}
+	//f, err := os.Open("./temp.txt")
+	//if err != nil {
+	//	log.Fatal(err.Error())
+	//} else {
+	//	fmt.Println(f.Name())
+	//}
 
-	er1 := do()
-	fmt.Println(er1)
-	er2 := do2()
-	fmt.Println(er2)
+	a := 10
+	b := 20
+	res, err := isSame(a, b)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(res)
+	}
 }
