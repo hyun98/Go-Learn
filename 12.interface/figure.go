d0d1f45c9f18e4aa90fca437eb067c092477cb5a
hyun98 <hyun0404woo@gmail.com>
2024-01-12 00:57:48 +0900

    Rect 구조체 패키지 내 중복 선언 수정

--- 12.interface/figure.go
+++ 12.interface/figure.go
@@ -15,8 +15,7 @@ type Cylinder struct {
 	height float64
 }
 
-
-type Rect struct {
+type Rect2 struct {
 	side [3]float64
 }
 
@@ -28,19 +27,19 @@ func (c Cylinder) volume() float64 {
 	return math.Pi * c.radius * c.radius * c.height
 }
 
-func (r Rect) surfaceArea() float64 {
-	return 2 * (r.side[0] * r.side[1] + r.side[0] * r.side[2] + r.side[1] * r.side[2])
+func (r Rect2) surfaceArea() float64 {
+	return 2 * (r.side[0]*r.side[1] + r.side[0]*r.side[2] + r.side[1]*r.side[2])
 }
 
-func (r Rect) volume() float64 {
+func (r Rect2) volume() float64 {
 	return r.side[0] * r.side[1] * r.side[2]
 }
 
 func main() {
 	cy1 := Cylinder{radius: 10, height: 10}
 	cy2 := Cylinder{radius: 4.2, height: 15.6}
-	cu1 := Rect{side: [3]float64{10.5, 20.2, 20}}
-	cu2 := Rect{side: [3]float64{4, 10, 23}}
+	cu1 := Rect2{side: [3]float64{10.5, 20.2, 20}}
+	cu2 := Rect2{side: [3]float64{4, 10, 23}}
 
 	printMeasure(cy1, cy2, cu1, cu2)
 }
